6da9fe4a1df8775518f26737680cc76636439926
Shark <sharktheone@proton.me>
2025-08-05 18:11:00 +0200

    add utils with pausable for later

---
+++ crates/micro_utils/src/lib.rs
@@ -0,0 +1,4 @@
+mod pausable;
+
+pub use pausable::*;
+
---
+++ crates/micro_utils/src/pausable.rs
@@ -0,0 +1,8 @@
+use std::error::Error;
+
+pub trait Pausable: Sized {
+    type Error: Error;
+    
+    fn pause(self) -> Vec<u8>;
+    fn resume(buf: Vec<u8>) -> Result<Self, Self::Error>;
+}
