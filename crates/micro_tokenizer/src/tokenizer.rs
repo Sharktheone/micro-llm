6ecee44d3a039f96f7e9bd8a9da973e0ab51a266
Shark <sharktheone@proton.me>
2025-08-06 04:35:26 +0200

    implement tokenizer decoding

--- crates/micro_tokenizer/src/tokenizer.rs
+++ crates/micro_tokenizer/src/tokenizer.rs
@@ -13,18 +13,42 @@ pub struct Tokenizer {
 impl Tokenizer {
     pub fn from_vocab(vocab: &[Vec<u8>]) -> Self {
         todo!()
-        
+
     }
-    
+
     pub fn from_unordered_vocab(vocab: &[(Vec<u8>, TokenId)]) -> Self {
         todo!()
     }
-    
+
     pub fn encode(&self, text: impl AsRef<[u8]>) -> Vec<TokenId> {
         Vec::new()
     }
-    
-    pub fn decode(&self, tokens: &[TokenId]) -> Vec<u8> {
-        Vec::new()
+
+    pub fn decode(&self, tokens: &[TokenId]) -> String {
+        let bytes = self.decode_bytes(tokens);
+
+        String::from_utf8_lossy(&bytes).into_owned()
+    }
+
+    pub fn decode_bytes(&self, tokens: &[TokenId]) -> Vec<u8> {
+        let mut bytes = Vec::with_capacity(tokens.len() * 6);
+
+        for token in tokens {
+            let Some(range) = self.vocab.get(*token as usize) else {
+                continue;
+            };
+
+            let Some(voc) = self.expanded.get(range.clone()) else {
+                continue;
+            };
+
+
+            bytes.extend_from_slice(voc);
+        }
+
+
+        bytes
+
+
     }
 }
