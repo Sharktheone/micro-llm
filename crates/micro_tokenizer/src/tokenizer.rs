597665664c67bb88a12b0f3de55603c2bd12685c
Shark <sharktheone@proton.me>
2025-08-06 18:58:22 +0200

    add search max token

--- crates/micro_tokenizer/src/tokenizer.rs
+++ crates/micro_tokenizer/src/tokenizer.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 
 type TokenId = u32;
 
@@ -120,6 +121,210 @@ impl Tokenizer {
 
 
         bytes
+    }
+    
+    fn search_max_token(&self, buf: &[u8]) -> (u32, usize) {
+        let Some(pref) = buf.first().copied() else {
+            return (u32::MAX, 0);
+        };
+
+        let range = self.cached_range[pref as usize];
+
+        let tokens = self.tokens.get_range(range);
+
+        if tokens.is_empty() {
+            return (u32::MAX, 1);
+        }
+
+        let Some(mut search) = buf.get(1).cloned() else {
+            let buf = self.expanded.get_range(tokens[0].0);
+            if buf[0] == pref  && buf.len() == 1 {
+                return (tokens[0].1, 1);
+            }
+            
+            //TODO: we should now search for the next few tokens
+            return (u32::MAX, 1);
+        };
+
+        let mut left = 0;
+        let mut right = tokens.len() as u32;
+        let mut round = 1;
+        let mut size = tokens.len() as u32;
+        let mut cur_token = tokens[0].1;
+        let mut cur_len = 1;
+
+        let mut real_tok_idx = None;
+
+        let ret = {
+            #[inline(always)]
+            |cur_token: u32, cur_len: usize, real_tok_idx: Option<u32>| {
+                if let Some(idx) = real_tok_idx {
+                    if let Some((range, tok)) = tokens.get(idx as usize) {
+                        let expanded = self.expanded.get_range(*range);
+
+                        if buf.len() > expanded.len() && &buf[..expanded.len()] == expanded {
+                            return (*tok, expanded.len());
+                        }
+                    }
+                }
+
+                (cur_token, cur_len)
+            }
+        };
+
+        while left < right {
+            let mid = left + size / 2;
+
+            let (range, tok) = tokens[mid as usize];
+
+            let expanded = self.expanded.get_range(range);
+
+            // if we don't have any here, this means that the token is less than our search
+            let a = expanded.get(round).cloned();
+
+            let Some(a) = a else {
+                left = mid + 1;
+
+                size = right - left;
+
+                continue;
+            };
+
+            let cmp = a.cmp(&search);
+
+            match cmp {
+                Ordering::Equal => {
+                    let mut size = mid - left;
+
+                    let mut left_right = mid;
+
+                    //search for the new left bound
+                    while left < left_right {
+                        let mid = left + size / 2;
+
+                        let (a, _) = tokens[mid as usize];
+
+                        let expanded = self.expanded.get_range(a);
+
+                        let Some(a) = expanded.get(round).cloned() else {
+                            left = mid + 1;
+                            size = left_right - left;
+                            continue;
+                        };
+
+                        // `a` can only be less or equal search
+                        if a < search {
+                            left = mid + 1;
+                            size = left_right - left;
+                            continue;
+                        }
+
+                        // we checked that `a` isn't less  than search, so it must be equal
+
+                        let (b, _) = tokens[mid as usize - 1];
+
+                        let expanded = self.expanded.get_range(b);
+                        let b = expanded.get(round).cloned();
+
+                        // if b is none or, it means we have found our location
+                        let Some(b) = b else {
+                            left = mid;
+                            break;
+                        };
+
+                        // if b is less than search, we have found our location
+                        if b < search {
+                            left = mid;
+                            break;
+                        }
+
+                        // b is less than search, so we need to move left
+
+                        left_right = mid;
+                        size = left_right - left;
+                    }
+
+                    // search for the new right bound
+                    let mut size = right - mid;
+                    let mut right_left = mid;
+                    while right_left < right {
+                        let mid = right_left + size / 2;
+
+                        let (a, _) = tokens[mid as usize - 1];
+
+                        let expanded = self.expanded.get_range(a);
+
+                        let Some(a) = expanded.get(round).cloned() else {
+                            // `a` is none, we are too far
+                            right = mid;
+                            size = right - right_left;
+                            continue;
+                        };
+
+                        // `a` can only be greater or equal search
+                        if a > search {
+                            // `a` is greater than search, we are too far
+                            right = mid;
+                            size = right - right_left;
+                            continue;
+                        }
+
+                        // we checked that `a` isn't greater than search, so it must be equal
+
+                        let (b, _) = tokens[mid as usize];
+
+                        let expanded = self.expanded.get_range(b);
+                        let b = expanded.get(round).cloned();
+
+                        // if b is none, it means we have found our location
+                        let Some(b) = b else {
+                            right = mid;
+                            break;
+                        };
+
+                        // if b is greater than search, we have found our location
+                        if b > search {
+                            right = mid;
+                            break;
+                        }
+
+                        // b is equal to search, so we need to move right
+
+                        right_left = mid + 1;
+                        size = right - right_left;
+                    }
+                    
+                    round += 1;
+                    if round == range.len() as usize {
+                        cur_token = tok;
+                        cur_len = round;
+                        real_tok_idx = None;
+                    } else {
+                        let (range, tok) = tokens[left as usize];
+
+                        if range.len() as usize == round {
+                            cur_token = tok;
+                            cur_len = round;
+                            real_tok_idx = None;
+                        } else {
+                            real_tok_idx = Some(mid)
+                        }
+                    }
+
+                    let Some(s) = buf.get(round).cloned() else {
+                        return ret(cur_token, cur_len, real_tok_idx);
+                    };
+
+                    search = s;
+                }
+                Ordering::Less => left = mid + 1,
+                Ordering::Greater => right = mid,
+            }
+
+            size = right - left;
+        }
+
+        ret(cur_token, cur_len, real_tok_idx)
     }
 }
 
