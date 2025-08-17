22d108f910540715640d08e354740f77a521ed65
Shark <sharktheone@proton.me>
2025-08-17 12:51:19 +0200

    add Llama Model def

--- crates/micro_executor/src/model/llama.rs
+++ crates/micro_executor/src/model/llama.rs
@@ -1,3 +1,33 @@
-pub struct LlamaModel {
-    //TODO
+use crate::nn::{Embedding, Linear, RmsNorm};
+
+pub struct LlamaModel<'a, T> {
+    embedding: Embedding<'a, T>,
+    blocks: Vec<LlamaBlock<'a, T>>,
+    ln_f: RmsNorm<'a, T>,
+    lm_head: Linear<'a, T>,
+}
+
+pub struct LlamaBlock<'a, T> {
+    attn: LlamaAttention<'a, T>,
+    mlp: LlamaMlp<'a, T>,
+    ln_1: RmsNorm<'a, T>,
+    ln_2: RmsNorm<'a, T>,
+}
+
+
+pub struct LlamaAttention<'a, T> {
+    q_proj: Linear<'a, T>,
+    k_proj: Linear<'a, T>,
+    v_proj: Linear<'a, T>,
+    o_proj: Linear<'a, T>,
+    num_heads: usize,
+    num_kv_heads: usize,
+    head_dim: usize,
+    max_pos_emb: usize,
+}
+
+pub struct LlamaMlp<'a, T> {
+    c_fc1: Linear<'a, T>,
+    c_fc2: Linear<'a, T>,
+    c_proj: Linear<'a, T>,
 }
