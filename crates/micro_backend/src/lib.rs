14ffe8ac172870ba8f1452e948e17f68a5efeb52
Shark <sharktheone@proton.me>
2025-08-25 23:32:43 +0200

    think more about tensor, dtype and so on

--- crates/micro_backend/src/lib.rs
+++ crates/micro_backend/src/lib.rs
@@ -1,8 +1,17 @@
 
 
 
-pub trait Backend {
-    type Linear;
-    type Embedding;
-    type LayerNorm;
+pub trait Backend: Sized {
+    type Tensor<'a, T: DType<Self>>: Tensor<'a, T, Self>;
+}
+
+pub trait DType<B: Backend> {}
+
+pub trait Tensor<'a, T: DType<B>, B: Backend>: Sized {
+    fn shape(&self) -> &[usize];
+    fn data(&self) -> &[T];
+
+    fn t(&self) -> Self;
+
+    fn to_dtype<U: DType<B>>(&self) -> B::Tensor<'_, U>;
 }
