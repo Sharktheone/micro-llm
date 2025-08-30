cba4aa1c518883c772073347ecd8a8b381567127
Shark <sharktheone@proton.me>
2025-08-30 20:03:07 +0200

    new tensor type

--- crates/micro_backend/src/lib.rs
+++ crates/micro_backend/src/lib.rs
@@ -1,17 +1,26 @@
 
 
+use std::ops::{Add, Mul};
 
 pub trait Backend: Sized {
-    type Tensor<'a, T: DType<Self>>: Tensor<'a, T, Self>;
+    type RefStore: Store<Self>;
+    type OwnedStore: Store<Self>;
+    type LoadStore: Store<Self>;
+    type SharedStore: Store<Self>;
+
+    type Tensor<'a, T: DType<Self>, S: Store<Self>, D: Dim<Self>>: Tensor<'a, T, Self, S, D>;
 }
 
 pub trait DType<B: Backend> {}
 
-pub trait Tensor<'a, T: DType<B>, B: Backend>: Sized {
+pub trait Store<B: Backend> {}
+pub trait Dim<B: Backend> {}
+
+pub trait Tensor<'a, T: DType<B>, B: Backend, S: Store<B>, D: Dim<B>>: Sized + Add + Mul {
     fn shape(&self) -> &[usize];
     fn data(&self) -> &[T];
 
     fn t(&self) -> Self;
 
-    fn to_dtype<U: DType<B>>(&self) -> B::Tensor<'_, U>;
+    fn to_dtype<U: DType<B>>(&self) -> B::Tensor<'_, U, B::OwnedStore, D>;
 }
