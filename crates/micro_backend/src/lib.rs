6de23019882b1cc9e4d3ed4eddca70eded8a7288
Shark <sharktheone@proton.me>
2025-08-30 22:38:57 +0200

    more tensor stuff

--- crates/micro_backend/src/lib.rs
+++ crates/micro_backend/src/lib.rs
@@ -23,11 +23,14 @@ pub trait DType: Display + Debug + Copy {}
 pub trait Store<B: Backend> {}
 pub trait Dim<B: Backend> {}
 
-pub trait Tensor<'a, T: DType, B: Backend + SupportsDType<T>, S: Store<B>, D: Dim<B>>: Sized + Add + Mul {
+pub trait Tensor<'a, T: DType, B: Backend + SupportsDType<T>, S: Store<B>, D: Dim<B>>: Sized + Debug + Clone {
     fn shape(&self) -> &[usize];
     fn data(&self) -> &[T];
 
-    fn t(&self) -> Self;
+    fn t(&self) -> B::Tensor<'a, T, B::RefStore, D>;
 
     fn to_dtype<U: DType>(&self) -> B::Tensor<'_, U, B::OwnedStore, D> where B: SupportsDType<U>;
+
+    fn add(&self, other: &B::Tensor<'a, T, S, D>) -> B::Tensor<'_, T, B::OwnedStore, D>;
+    fn mul(&self, other: &B::Tensor<'a, T, S, D>) -> B::Tensor<'_, T, B::OwnedStore, D>;
 }
