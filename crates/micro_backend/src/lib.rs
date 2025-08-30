6ef83bb2cd1395f4a0879a4824d96811accfab4b
Shark <sharktheone@proton.me>
2025-08-30 20:53:47 +0200

    better dtype stuff

--- crates/micro_backend/src/lib.rs
+++ crates/micro_backend/src/lib.rs
@@ -1,5 +1,6 @@
 mod tensors;
 
+use std::fmt::{Debug, Display};
 pub use tensors::*;
 
 use std::ops::{Add, Mul};
@@ -10,19 +11,23 @@ pub trait Backend: Sized {
     type LoadStore: Store<Self>;
     type SharedStore: Store<Self>;
 
-    type Tensor<'a, T: DType<Self>, S: Store<Self>, D: Dim<Self>>: Tensor<'a, T, Self, S, D>;
+    type Tensor<'a, T: DType, S: Store<Self>, D: Dim<Self>>: Tensor<'a, T, Self, S, D> where Self: SupportsDType<T>;
 }
 
-pub trait DType<B: Backend> {}
+
+
+pub trait SupportsDType<D: DType>: Backend {}
+
+pub trait DType: Display + Debug + Copy {}
 
 pub trait Store<B: Backend> {}
 pub trait Dim<B: Backend> {}
 
-pub trait Tensor<'a, T: DType<B>, B: Backend, S: Store<B>, D: Dim<B>>: Sized + Add + Mul {
+pub trait Tensor<'a, T: DType, B: Backend + SupportsDType<T>, S: Store<B>, D: Dim<B>>: Sized + Add + Mul {
     fn shape(&self) -> &[usize];
     fn data(&self) -> &[T];
 
     fn t(&self) -> Self;
 
-    fn to_dtype<U: DType<B>>(&self) -> B::Tensor<'_, U, B::OwnedStore, D>;
+    fn to_dtype<U: DType>(&self) -> B::Tensor<'_, U, B::OwnedStore, D> where B: SupportsDType<U>;
 }
