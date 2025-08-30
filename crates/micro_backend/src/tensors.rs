2a33ed4ae12e44f50e6414b811a4e283a6d71923
Shark <sharktheone@proton.me>
2025-08-30 22:36:18 +0200

    make tensors pub

--- crates/micro_backend/src/tensors.rs
+++ crates/micro_backend/src/tensors.rs
@@ -1,20 +1,20 @@
-use crate::{Backend, DType};
+use crate::{Backend, DType, Dim, SupportsDType};
 
-type Tensor1<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::OwnedStore, Dim1<B>>;
-type Tensor2<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::OwnedStore, Dim2<B>>;
-type Tensor3<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::OwnedStore, Dim3<B>>;
+pub type Tensor1<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::OwnedStore, Dim1<B>>;
+pub type Tensor2<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::OwnedStore, Dim2<B>>;
+pub type Tensor3<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::OwnedStore, Dim3<B>>;
 
-type RefTensor1<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::RefStore, Dim1<B>>;
-type RefTensor2<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::RefStore, Dim2<B>>;
-type RefTensor3<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::RefStore, Dim3<B>>;
+pub type RefTensor1<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::RefStore, Dim1<B>>;
+pub type RefTensor2<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::RefStore, Dim2<B>>;
+pub type RefTensor3<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::RefStore, Dim3<B>>;
 
-type LoadTensor1<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::LoadStore, Dim1<B>>;
-type LoadTensor2<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::LoadStore, Dim2<B>>;
-type LoadTensor3<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::LoadStore, Dim3<B>>;
+pub type LoadTensor1<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::LoadStore, Dim1<B>>;
+pub type LoadTensor2<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::LoadStore, Dim2<B>>;
+pub type LoadTensor3<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::LoadStore, Dim3<B>>;
 
-type SharedTensor1<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::SharedStore, Dim1<B>>;
-type SharedTensor2<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::SharedStore, Dim2<B>>;
-type SharedTensor3<'a, B: Backend, T: DType<B>> = B::Tensor<'a, T, B::SharedStore, Dim3<B>>;
+pub type SharedTensor1<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::SharedStore, Dim1<B>>;
+pub type SharedTensor2<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::SharedStore, Dim2<B>>;
+pub type SharedTensor3<'a, B: Backend + SupportsDType<T>, T: DType> = B::Tensor<'a, T, B::SharedStore, Dim3<B>>;
 
 
 pub struct Dim1<B: Backend> {
