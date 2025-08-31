601f397dc7d3308a59b0b78b7902bac413679f50
Shark <sharktheone@proton.me>
2025-08-31 22:04:07 +0200

    new silu

--- crates/micro_executor/src/nn/silu.rs
+++ crates/micro_executor/src/nn/silu.rs
@@ -1,4 +1,5 @@
 use num_traits::{Float, FromPrimitive, One};
+use micro_backend::{Backend, DType, RefTensor2, SupportsDType, Tensor, Tensor2};
 
 pub fn silu<T: Float + FromPrimitive + One>(mut x: ndarray::Array2<T>) -> ndarray::Array2<T> {
     let one = T::one();
@@ -9,4 +10,22 @@ pub fn silu<T: Float + FromPrimitive + One>(mut x: ndarray::Array2<T>) -> ndarra
     });
 
     x
+}
+
+pub fn silu2<B: Backend + SupportsDType<T>, T: DType>(x: RefTensor2<B, T>) -> Tensor2<B, T> {
+    let one = T::one();
+
+    x.map_threaded(|v| {
+        let sigmoid = one / (one + (-v).exp());
+        v * sigmoid
+    })
+}
+
+pub fn silu_inplace<B: Backend + SupportsDType<T>, T: DType>(x: &mut Tensor2<B, T>) {
+    let one = T::one();
+
+    x.map_inplace_threaded(|v| {
+        let sigmoid = one / (one + (-v).exp());
+        v * sigmoid
+    });
 }
