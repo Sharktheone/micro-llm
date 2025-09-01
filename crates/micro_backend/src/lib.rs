e01ba019891fd36a230353a03be0d3b9cccb1a8a
Shark <sharktheone@proton.me>
2025-09-01 22:50:03 +0200

    change everything to supports

--- crates/micro_backend/src/lib.rs
+++ crates/micro_backend/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(warnings)]
 mod tensors;
 
 use std::fmt::{Debug, Display};
@@ -6,34 +7,41 @@ pub use tensors::*;
 use std::ops::{Add, Mul};
 use num_traits::{Float, FromPrimitive, One, Zero};
 
-pub trait Backend: Sized {
-    type RefStore: Store<Self>;
-    type OwnedStore: Store<Self>;
-    type LoadStore: Store<Self>;
-    type SharedStore: Store<Self>;
+pub trait Backend: Sized + SupportsStore<Self::RefStore> + SupportsStore<Self::OwnedStore> + SupportsStore<Self::LoadStore> + SupportsStore<Self::SharedStore> {
+    type RefStore: Store;
+    type OwnedStore: Store;
+    type LoadStore: Store;
+    type SharedStore: Store;
 
-    type Tensor<'a, T: DType, S: Store<Self>, D: Dim<Self>>: Tensor<'a, T, Self, S, D> where Self: SupportsDType<T>;
+    type Tensor<'a, T: DType, S: Store, D: Dim>: Tensor<'a, T, Self, S, D> where Self: Supports<T, S, D>;
 }
 
 
+pub trait SupportsDType<T: DType> {}
+pub trait SupportsStore<S: Store> {}
+pub trait SupportsDim<D: Dim> {}
 
-pub trait SupportsDType<D: DType>: Backend {}
+
+pub trait Supports<T: DType, S: Store, D: Dim>: SupportsDType<T> + SupportsStore<S> + SupportsDim<D> {}
+
+impl <B: SupportsDType<T> + SupportsStore<S> + SupportsDim<D>, T: DType, S: Store, D: Dim> Supports<T, S, D> for B {}
+
 
 pub trait DType: 'static + Display + Debug + Copy + Float + FromPrimitive + Zero + One + Send + Sync {}
 
-pub trait Store<B: Backend> {}
-pub trait Dim<B: Backend> {
-    type Larger: Dim<B>;
-    type Smaller: Dim<B>;
+pub trait Store {}
+pub trait Dim {
+    type Larger: Dim;
+    type Smaller: Dim;
 }
 
 
 
-pub trait OwnedTensor<B: Backend, T: DType, D: Dim<B>> {}
+pub trait OwnedTensor<B: Backend, T: DType, D: Dim> {}
 
-impl<B: Backend + SupportsDType<T>, T: DType, D: Dim<B>> OwnedTensor<B, T, D> for B::Tensor<'_, T, B::OwnedStore, D> {}
+impl<B: Backend + Supports<T, B::OwnedStore, D>, T: DType, D: Dim> OwnedTensor<B, T, D> for B::Tensor<'_, T, B::OwnedStore, D> {}
 
-pub trait Tensor<'a, T: DType, B: Backend + SupportsDType<T>, S: Store<B>, D: Dim<B>>: Sized + Debug + Clone {
+pub trait Tensor<'a, T: DType, B: Backend + Supports<T, S, D>, S: Store, D: Dim>: Sized + Debug + Clone {
     fn shape(&self) -> &[usize];
     fn data(&self) -> &[T];
 
@@ -43,11 +51,11 @@ pub trait Tensor<'a, T: DType, B: Backend + SupportsDType<T>, S: Store<B>, D: Di
     fn to_owned<'b>(&self) -> B::Tensor<'b, T, B::OwnedStore, D>;
     fn as_ref(&self) -> B::Tensor<'a, T, B::RefStore, D>;
 
-    fn add<'b, S2: Store<B>>(&self, other: &B::Tensor<'_, T, S2, D>) -> B::Tensor<'b, T, B::OwnedStore, D>;
-    fn mul<'b, S2: Store<B>>(&self, other: &B::Tensor<'_, T, S2, D>) -> B::Tensor<'b, T, B::OwnedStore, D>;
+    fn add<'b, S2: Store>(&self, other: &B::Tensor<'_, T, S2, D>) -> B::Tensor<'b, T, B::OwnedStore, D> where B: SupportsStore<S2>;
+    fn mul<'b, S2: Store>(&self, other: &B::Tensor<'_, T, S2, D>) -> B::Tensor<'b, T, B::OwnedStore, D> where B: SupportsStore<S2>;
 
-    fn mul_inplace<S2: Store<B>>(&mut self, other: &B::Tensor<'_, T, S2, D>) where Self: OwnedTensor<B, T, D>;
-    fn add_inplace<S2: Store<B>>(&mut self, other: &B::Tensor<'_, T, S2, D>) where Self: OwnedTensor<B, T, D>;
+    fn mul_inplace<S2: Store>(&mut self, other: &B::Tensor<'_, T, S2, D>) where Self: OwnedTensor<B, T, D>, B: SupportsStore<S2>;
+    fn add_inplace<S2: Store>(&mut self, other: &B::Tensor<'_, T, S2, D>) where Self: OwnedTensor<B, T, D>, B: SupportsStore<S2>;
 
     fn map(&self, f: impl Fn(T) -> T) -> B::Tensor<'a, T, B::OwnedStore, D>;
     fn map_inplace(&mut self, f: impl Fn(T) -> T) where Self: OwnedTensor<B, T, D>;
@@ -57,6 +65,13 @@ pub trait Tensor<'a, T: DType, B: Backend + SupportsDType<T>, S: Store<B>, D: Di
 
     fn map_batched(&self, f: impl Fn(&[T], &mut [T]) + Send + Sync, batch_size: usize) -> B::Tensor<'a, T, B::OwnedStore, D>;
     fn map_inplace_batched(&mut self, f: impl Fn(&mut [T]) + Send + Sync, batch_size: usize) where Self: OwnedTensor<B, T, D>;
+
+    fn select(&self, indices: &[usize]) -> B::Tensor<'a, T, B::OwnedStore, D>;
+
+    fn select_from_start(&self, count: usize) -> B::Tensor<'a, T, B::RefStore, D>;
+
+    fn slice(&self, ranges: &[std::ops::Range<usize>]) -> B::Tensor<'a, T, B::OwnedStore, D>;
+
 }
 
 // pub trait OwnedTensor<'a, T: DType, B: Backend + SupportsDType<T>, D: Dim<B>>: Tensor<'a, T, B, B::OwnedStore, D> {
