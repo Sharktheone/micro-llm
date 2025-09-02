f1cb1ef831c261a72fd407846a55180bed1f8c5d
Shark <sharktheone@proton.me>
2025-09-02 03:11:41 +0200

    store stuff

---
+++ crates/micro_backend/src/store.rs
@@ -0,0 +1,22 @@
+
+pub trait Store: private::Sealed {}
+
+pub struct RefStore;
+pub struct OwnedStore;
+pub struct LoadStore;
+pub struct SharedStore;
+
+impl Store for RefStore {}
+impl Store for OwnedStore {}
+impl Store for LoadStore {}
+impl Store for SharedStore {}
+
+
+impl private::Sealed for RefStore {}
+impl private::Sealed for OwnedStore {}
+impl private::Sealed for LoadStore {}
+impl private::Sealed for SharedStore {}
+
+mod private {
+    pub trait Sealed {}
+}
