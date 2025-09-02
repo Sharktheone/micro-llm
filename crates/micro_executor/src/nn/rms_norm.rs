cc378c0567d6d81c3579637de3aa375ffee7d40f
Shark <sharktheone@proton.me>
2025-09-02 04:20:28 +0200

    rms norm with new backend

--- crates/micro_executor/src/nn/rms_norm.rs
+++ crates/micro_executor/src/nn/rms_norm.rs
@@ -1,6 +1,8 @@
 use std::fmt::{Debug, Display};
 use ndarray::{Array2, ArrayView1, ScalarOperand, Axis};
 use num_traits::{AsPrimitive, Float, FromPrimitive, Zero};
+use rayon::slice::{ParallelSlice, ParallelSliceMut};
+use micro_backend::{Backend, DType, Dim, LoadTensor1, RefTensor2, Tensor, Tensor2};
 use crate::load::{load_array1, Loadable};
 
 pub struct RmsNorm<'a, T> {
@@ -65,5 +67,53 @@ impl<'a, T: Loadable> RmsNorm<'a, T> {
         let weight = load_array1(model, &format!("{}weight", prefix))?;
 
         Ok(RmsNorm { weight, eps })
+    }
+}
+
+pub struct RmsNorm2<'a, B: Backend, T: DType> {
+    pub weight: LoadTensor1<'a, B, T>,
+    pub eps: f32,
+}
+
+impl<'a,  B: Backend, T: DType> RmsNorm2<'a, B, T> {
+    pub fn forward(&self, x: &RefTensor2<'_, B, T>) -> anyhow::Result<Tensor2<'_, B, T>> {
+        let [rows, cols] = x.dim().pattern();
+        
+        if rows == 0 || cols == 0 {
+            anyhow::bail!("RmsNorm: input has zero-sized dimension: ({rows}, {cols})");
+        }
+        if self.weight.len() != cols {
+            anyhow::bail!(
+                "RmsNorm: weight length {} does not match input cols {}",
+                self.weight.len(),
+                cols
+            );
+        }
+
+        let x_view = x.data();
+        let weight_view = self.weight.data();
+
+        let mut output = vec![T::zero(); x_view.len()];
+
+        x_view.par_chunks(cols)
+            .zip(output.par_chunks_mut(cols))
+            .for_each(|(src, dst)| {
+                let sum2 = src
+                    .iter()
+                    .map(|&v| {
+                        let v = v.as_();
+                        v * v
+                    })
+                    .sum::<f32>();
+
+                let m = (sum2 / cols as f32 + self.eps).sqrt();
+
+                let m = T::from_f32(m).unwrap_or_else(T::nan);
+                for ((d, s), alpha) in dst.iter_mut().zip(src.iter()).zip(weight_view) {
+                    *d = *s / m * *alpha
+                }
+            });
+
+        Ok(Tensor2::<B, T>::from_vec((rows, cols), output)?)
     }
 }
