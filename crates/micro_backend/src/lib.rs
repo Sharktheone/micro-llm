913e5e181aea9213a04eedc89dab36b9817d1943
Shark <sharktheone@proton.me>
2025-09-03 07:11:16 +0200

    tensor insert/remove axis and (un)squeeze ops

--- crates/micro_backend/src/lib.rs
+++ crates/micro_backend/src/lib.rs
@@ -89,8 +89,14 @@ pub trait Tensor<'a, T: DType, B: Backend + SupportsDType<T>, S: Store, D: Dim>:
 
 
     fn slice(&self, ranges: &[std::ops::Range<usize>]) -> B::Tensor<'a, T, OwnedStore, D>;
-    
+
     fn from_slice(data: &'a [T], shape: impl Into<D>) -> B::Tensor<'a, T, RefStore, D>;
-    
+
     fn from_vec(data: Vec<T>, shape: impl Into<D>) -> B::Tensor<'a, T, OwnedStore, D>;
+    
+    fn squeeze(&self, axis: usize) -> B::Tensor<'a, T, RefStore, D::Smaller>;
+    fn unsqueeze(&self, axis: usize) -> B::Tensor<'a, T, RefStore, D::Larger>;
+    
+    fn insert_axis(self, axis: usize) -> B::Tensor<'a, T, S, D::Larger>;
+    fn remove_axis(self, axis: usize) -> B::Tensor<'a, T, S, D::Smaller>;
 }
