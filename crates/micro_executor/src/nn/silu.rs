b77ce3536ac66ae75f24d6039fbcde19b4980056
Shark <sharktheone@proton.me>
2025-09-05 23:54:39 +0200

    move silu

--- crates/micro_executor/src/nn/silu.rs
+++ crates/micro_executor/src/nn/silu.rs
@@ -1,18 +1,7 @@
 use micro_backend::{Backend, DType, RefTensor2, SupportsDType, Tensor, Tensor2};
 use num_traits::{Float, FromPrimitive, One};
 
-pub fn silu<T: Float + FromPrimitive + One>(mut x: ndarray::Array2<T>) -> ndarray::Array2<T> {
-    let one = T::one();
-
-    x.mapv_inplace(|v| {
-        let sigmoid = one / (one + (-v).exp());
-        v * sigmoid
-    });
-
-    x
-}
-
-pub fn silu2<B: Backend + SupportsDType<T>, T: DType>(x: RefTensor2<B, T>) -> Tensor2<B, T> {
+pub fn silu_backend<B: Backend + SupportsDType<T>, T: DType>(x: RefTensor2<B, T>) -> Tensor2<B, T> {
     let one = T::one();
 
     x.map_threaded(|v| {
@@ -21,7 +10,7 @@ pub fn silu2<B: Backend + SupportsDType<T>, T: DType>(x: RefTensor2<B, T>) -> Te
     })
 }
 
-pub fn silu_inplace<B: Backend + SupportsDType<T>, T: DType>(x: &mut Tensor2<B, T>) {
+pub fn silu_inplace_backend<B: Backend + SupportsDType<T>, T: DType>(x: &mut Tensor2<B, T>) {
     let one = T::one();
 
     x.map_inplace_threaded(|v| {
