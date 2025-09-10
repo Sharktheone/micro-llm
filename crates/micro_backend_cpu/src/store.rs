9e7d71493ce0522246ed3ed96b3ed3cee79d6241
Shark <sharktheone@proton.me>
2025-09-10 23:28:46 +0200

    make cpustore send and sync

--- crates/micro_backend_cpu/src/store.rs
+++ crates/micro_backend_cpu/src/store.rs
@@ -2,27 +2,27 @@ use micro_backend::{LoadStore, OwnedStore, RefStore, SharedStore, Store};
 use std::ops::Deref;
 
 pub trait CpuStore {
-    type DataStorage<'a, T: 'a>: AsRef<[T]>;
+    type DataStorage<'a, T: 'a + Send + Sync>: AsRef<[T]> + Send + Sync;
 }
 
 impl<T: Store> CpuStore for T
 where
     T: Store,
 {
-    default type DataStorage<'a, U: 'a> = &'a [U];
+    default type DataStorage<'a, U: 'a + Send + Sync> = &'a [U];
 }
 
 impl CpuStore for RefStore {
-    type DataStorage<'a, U: 'a> = &'a [U];
+    type DataStorage<'a, U: 'a + Send + Sync> = &'a [U];
 }
 impl CpuStore for OwnedStore {
-    type DataStorage<'a, U: 'a> = Vec<U>;
+    type DataStorage<'a, U: 'a + Send + Sync> = Vec<U>;
 }
 
 impl CpuStore for LoadStore {
-    type DataStorage<'a, U: 'a> = &'a [U];
+    type DataStorage<'a, U: 'a + Send + Sync> = &'a [U];
 }
 
 impl CpuStore for SharedStore {
-    type DataStorage<'a, U: 'a> = &'a [U];
+    type DataStorage<'a, U: 'a + Send + Sync> = &'a [U];
 }
