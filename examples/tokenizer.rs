91ccd95a31d87aeb82d0fc9bc2db5e508ae7cffa
Shark <sharktheone@proton.me>
2025-09-30 16:45:28 +0200

    fix args in tokenizer example

--- examples/tokenizer.rs
+++ examples/tokenizer.rs
@@ -2,17 +2,18 @@ use micro_tokenizer::hf::load_hf_tokenizer;
 
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
-    if args.len() != 2 || (args.len() == 3 && args[2] != "--file") {
+    if args.len() != 3 && (args.len() != 4 && args[2] != "--file") {
+        dbg!(&args, &args[2], args.len());
         eprintln!("Usage: {} <tokenizer> <text>", args[0]);
         eprintln!("Usage: {} <tokenizer> --file <textfile>", args[0]);
         std::process::exit(1);
     }
 
     let path = &args[1];
-    let hf = load_hf_tokenizer(&path).unwrap();
+    let hf = load_hf_tokenizer(path).unwrap();
 
-    let text = if args.len() == 3 {
-        std::fs::read_to_string(&args[2]).unwrap()
+    let text = if args.len() == 4 {
+        std::fs::read_to_string(&args[3]).unwrap()
     } else {
         args[2].clone()
     };
